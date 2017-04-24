c022ab12ab57817d5e4f0af37b888d35381a48a9
Sanjaya Gajurel <sxg125@users.noreply.github.com>
2017-04-24 13:11:12 -0400

    Create build_container_cuda7.5.sh

---
+++ build_container_cuda7.5.sh
@@ -0,0 +1,25 @@
+#Created by Sanjaya Gajurel -
+#References:
+#1. https://github.com/clemsonciti/singularity-images/tree/master/tensorflow
+#2. http://singularity.lbl.gov/docs-docker
+
+# Remove the image first
+#sudo rm -f ubuntu_tensorflow_gpu.img
+
+#Create tensorflow image
+sudo /usr/local/singularity/2.2.1/bin/singularity create tensorflow.img
+sudo /usr/local/singularity/2.2.1/bin/singularity expand --size 8192 tensorflow.img
+
+# Import tensorflow image from docker
+#sudo /usr/local/singularity/2.2.1/bin/singularity bootstrap tensorflow.img ubuntu.def
+sudo /usr/local/singularity/2.2.1/bin/singularity import tensorflow.img docker://tensorflow/tensorflow:latest
+sudo /usr/local/singularity/2.2.1/bin/singularity import tensorflow.img docker://tensorflow/tensorflow:latest-gpu
+
+# Install and run CUDA driver
+sudo /usr/local/singularity/2.2.1/bin/singularity exec -B `pwd`:/mnt -w tensorflow.img sh /mnt/install_cuda.sh
+#sudo /usr/local/singularity/2.2.1/bin/singularity exec -B `pwd`:/mnt -w tensorflow.img sh /mnt/install_tensorflow.sh
+
+# Create mount points
+sudo /usr/local/singularity/2.2.1/bin/singularity exec -B `pwd`:/mnt -w tensorflow.img mkdir /local_scratch
+sudo /usr/local/singularity/2.2.1/bin/singularity exec -B `pwd`:/mnt -w tensorflow.img mkdir /scratch
+sudo /usr/local/singularity/2.2.1/bin/singularity exec -B `pwd`:/mnt -w tensorflow.img mkdir /software
